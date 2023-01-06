c7c803d3c12476b7ba07ca0925f2a26ba01a1011
Josh <lutz_143@yahoo.com>
2023-01-06 13:50:19 -0500

    updates to questions and inquirer prompt

--- ToDo.md
+++ ToDo.md
@@ -2,7 +2,7 @@
 
 
 * WHEN I start the application
-* ~~team manager’s name, employee ID, email address, and office number~~
+* THEN I am prompted to enter the ~~team manager’s name, employee ID, email address, and office number~~
 
 * WHEN I enter the team manager’s name, employee ID, email address, and office number
 * THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
@@ -11,4 +11,4 @@
 * THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
 
 * WHEN I select the intern option
-* THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
+* THEN I am prompted to enter the `intern’s name, ID, email, and school, and I am taken back to the menu`
--- index.js
+++ index.js
@@ -1,25 +1,15 @@
-const managerGate = require("./lib/questions");
+const questions = require("./lib/questions");
+console.log(questions.internQuestions);
 
-// console.log(managerGate);
-
-/*
-WHEN I start the application
-THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-WHEN I enter the team manager’s name, employee ID, email address, and office number
-THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-WHEN I select the engineer option
-THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-WHEN I select the intern option
-THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-*/
 
 const inquirer = require("inquirer");
 
+
 const generateManagerGate = ({managerName, managerEmployeeId, managerEmployeeEmail, addTeammateOption}) =>
-`The manager is ${managerName} and their employee ID is ${managerEmployeeId} with an email of ${managerEmployeeEmail}.\nThe add teammate option selected was ${addTeammateOption}`;
+`The manager is ${managerName} and their employee ID is ${managerEmployeeId} with an email of ${managerEmployeeEmail}.`;
 
 const promptUser = () => {
-  return inquirer.prompt(managerGate);
+  return inquirer.prompt(questions.managerGate);
 }
 
 const init = () => {
